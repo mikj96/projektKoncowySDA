212b270d9392839b2995fc0a6ebc17bb4706af6c
mikj96 <mikolaj.plata@outlook.com>
2022-12-10 03:59:54 +0100

    menu update

--- projektKoncowy-wersja kluczowa.py
+++ projektKoncowy-wersja kluczowa.py
@@ -1,4 +1,4 @@
-#0. IMPORTY BIBLIOTEK
+#0. LIBRARY IMPORTS
 
 import numpy as np
 import pandas as pd
@@ -11,7 +11,6 @@ import plotly.express as px
 import plotly.graph_objs as go
 import os
 from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
@@ -31,84 +30,59 @@ from keras import models
 from keras import layers
 from keras import optimizers
 from keras.models import load_model
+register_matplotlib_converters()
 
-#0. ROBOCZE NAZWY MIESIĘCY + ŚCIEŻKA GDZIE POBRALIŚMY DANE
-
-months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
-              'november', 'december']
-filepath = fr"C:\\Users\Enter\OneDrive\Pulpit\smogData\\"
-
-#1. ZNAJDUJEMY WSZYSTKIE NAZWY SENSORÓW
-for month in months:
-    data = pd.read_csv(fr"{filepath}{month}-2017.csv")
-    sensors = []
-    for first_row_name in data.columns:
-        first_row_name
-        sensors.append(first_row_name[0:3])
-
-sensors = pd.unique(sensors)
-print('(1/5) All unique sensor names saved.')
-
-
-#2. ZAMIENIAMY NAZWY SENSORÓW NA INT - UNIKAMY NAZW LITEROWYCH
-real_sensors = []
-for value in sensors:
-    try:
-        real_sensors.append(int(value))
-    except ValueError:
-        pass
-print('(2/5) Changing sensor names to integer values completed.')
-
-'''
-#3. ZAPISYWANIE MIESIĘCZNYCH ODCZYTÓW DLA KAŻDEGO SENSORA
-for month in months:
-    data = pd.read_csv(fr"{filepath}{month}-2017.csv")
-    for sensor_name in real_sensors:
-        df_new = data[
-            [f'UTC time', f'{sensor_name}_temperature', f'{sensor_name}_humidity', f'{sensor_name}_pressure',
-             f'{sensor_name}_pm1', f'{sensor_name}_pm25',
-             f'{sensor_name}_pm10']]
-        df_new.to_csv(path_or_buf=fr"{filepath}sensors score\\{sensor_name}{month}.csv")
-print('(3/5) Saving data for each month, for each sensor Completed.')
-
-#4. MERGOWANIE MIESIĘCZNYCH TABEL W JEDNĄ ROCZNĄ DLA KAŻDEGO SENSORA
-months.pop(0)
-for sensor in real_sensors:
-    data = pd.read_csv(fr"{filepath}sensors score\\{sensor}january.csv")
+#1. SAVING MONTHLY READINGS FOR EACH SENSOR
+def saving_month_results():
     for month in months:
-        data_new = pd.read_csv(fr"{filepath}sensors score\\{sensor}{month}.csv")
-        data = pd.concat([data, data_new])
-    data.to_csv(path_or_buf=fr"{filepath}\\sensors score\\sensor{sensor}.csv")
-print('(4/5) Merging month results completed')
+        data = pd.read_csv(fr"{filepath}{month}-2017.csv")
+        for sensor_name in real_sensors:
+            df_new = data[
+                [f'UTC time', f'{sensor_name}_temperature', f'{sensor_name}_humidity', f'{sensor_name}_pressure',
+                 f'{sensor_name}_pm1', f'{sensor_name}_pm25',
+                 f'{sensor_name}_pm10']]
+            df_new.to_csv(path_or_buf=fr"{filepath}sensors score\\{sensor_name}{month}.csv")
+    print('(6/10) Saving data for each month, for each sensor Completed.')
 
-#5. USUWANIE DWÓCH NIEPOTRZEBNYCH KOLUMN, REDUKCJA DANYCH PRZEZ UŚREDNIENIE ODCZYTÓW DZIENNYCH
-for sensor in real_sensors:
-    data = pd.read_csv(fr"{filepath}\\sensors score\\sensor{sensor}.csv")
-    del data['Unnamed: 0.1']
-    del data['Unnamed: 0']
-    for record in data['UTC time']:
-        data = data.replace(record, record[:10])
-    data = data.groupby('UTC time').mean(numeric_only=True)
-    data.to_csv(path_or_buf=fr"{filepath}sensors score\\sensor{sensor}_mean.csv")
-print('(5/5) Saving mean values for each sensor completed.')
+#2. MERGING MONTHLY TABLES INTO ONE ANNUAL FOR EACH SENSOR
+def merging_year_data():
+    months.pop(0)
+    for sensor in real_sensors:
+        data = pd.read_csv(fr"{filepath}sensors score\\{sensor}january.csv")
+        for month in months:
+            data_new = pd.read_csv(fr"{filepath}sensors score\\{sensor}{month}.csv")
+            data = pd.concat([data, data_new])
+        data.to_csv(path_or_buf=fr"{filepath}\\sensors score\\sensor{sensor}.csv")
+    print('(7/10) Merging month results completed')
 
-#6. USUWANIE PLIKÓW POMOCNICZYCH
+#3. REMOVING TWO UNNECESSARY COLUMNS, REDUCING DATA BY AVERAGING DAILY READINGS
+def reducing_day_data():
+    for sensor in real_sensors:
+        data = pd.read_csv(fr"{filepath}\\sensors score\\sensor{sensor}.csv")
+        del data['Unnamed: 0.1']
+        del data['Unnamed: 0']
+        for record in data['UTC time']:
+            data = data.replace(record, record[:10])
+            data = data.groupby('UTC time').mean(numeric_only=True)
+        data.to_csv(path_or_buf=fr"{filepath}sensors score\\sensor{sensor}_mean.csv")
+    print('(8/10) Saving mean values for each sensor completed.')
 
-months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+#4. DELETING SUPPORT FILES
+def deleting_sup_files():
+    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
               'november', 'december']
-for sensor in real_sensors:
-    for month in months:
-        if os.path.exists(fr"{filepath}sensors score\\{sensor}{month}.csv"):
-            os.remove(fr"{filepath}sensors score\\{sensor}{month}.csv")
-print('(6/7) Deleting data for each month completed')
+    for sensor in real_sensors:
+        for month in months:
+            if os.path.exists(fr"{filepath}sensors score\\{sensor}{month}.csv"):
+                os.remove(fr"{filepath}sensors score\\{sensor}{month}.csv")
+    print('(9/10) Deleting data for each month completed')
 
-for sensor in real_sensors:
-    if os.path.exists(fr"{filepath}sensors score\\sensor{sensor}.csv"):
-        os.remove(fr"{filepath}sensors score\\sensor{sensor}.csv")
-print('(7/7) Deleting auxilary data completed.')
-'''
-7# RYSOWANIE WYKRESU
+    for sensor in real_sensors:
+        if os.path.exists(fr"{filepath}sensors score\\sensor{sensor}.csv"):
+            os.remove(fr"{filepath}sensors score\\sensor{sensor}.csv")
+    print('(10/10) Deleting auxilary data completed.')
 
+#5 DRAWING A GRAPH
 def plotting_concentration():
     plot_counter = 0
     for sensor in real_sensors:
@@ -118,21 +92,21 @@ def plotting_concentration():
                            parse_dates=True,
                            index_col='UTC time')
         dane = dane.dropna(how='any')
-        dane.rename(columns={f'{sensor}_temperature': 'Temperatura', f'{sensor}_humidity': 'Wilgotnosc'
-            , f'{sensor}_pressure': 'Cisnienie', f'{sensor}_pm1': 'Stezenie PM1', f'{sensor}_pm25': 'Stezenie PM25',
-                             f'{sensor}_pm10': 'Stezenie PM10'}, inplace=True)
+        dane.rename(columns={f'{sensor}_temperature': 'Temperature', f'{sensor}_humidity': 'Humidity'
+            , f'{sensor}_pressure': 'Pressure', f'{sensor}_pm1': 'Conc PM1', f'{sensor}_pm25': 'Conc PM25',
+                             f'{sensor}_pm10': 'Conc PM10'}, inplace=True)
         plt.close("all")
         fig, ax = plt.subplots()
         # zmienna_wykresu = str(input('temperature/humidity/pressure'))
-        ax.plot(dane.index, dane['Temperatura'], color='r')
+        ax.plot(dane.index, dane['Temperature'], color='r')
         ax.xaxis.set_tick_params(rotation=90)
 
         # rysujemy wykresy dla każdego stężenia PMI
         # color - kolor markera, linestyle - styl linii, label - nazwa krzywej; potrzebna do legendy
         ax2 = ax.twinx()
-        ax2.plot(dane.index, dane['Stezenie PM1'], color='b', linestyle='-', label='PMI 1')
-        ax2.plot(dane.index, dane['Stezenie PM25'], color='c', linestyle='-', label='PMI 2.5')
-        ax2.plot(dane.index, dane['Stezenie PM10'], color='g', linestyle='-', label='PMI 10')
+        ax2.plot(dane.index, dane['Conc PM1'], color='b', linestyle='-', label='PMI 1')
+        ax2.plot(dane.index, dane['Conc PM25'], color='c', linestyle='-', label='PMI 2.5')
+        ax2.plot(dane.index, dane['Conc PM10'], color='g', linestyle='-', label='PMI 10')
         ax2.legend()
         ax2.set_ylabel('PMI concentration')
         ax.set_title(f'Monthly graph of temperature changes and pollution in Cracow for sensor:{sensor}')
@@ -146,10 +120,8 @@ def plotting_concentration():
         ax.grid()
         plt.show()  # na razie zablokowane żeby nie rysowało wykresów
 
-#plotting_concentration()
-
-#8. PREDYKCJA
-#8.1. REGRESJA LINIOWA
+#6. PREDICTION
+#6.1. LINEAR REGRESSION
 def linear_regr(pmi_cat):
     r2_scorelist = {}
     counter = 0
@@ -160,13 +132,13 @@ def linear_regr(pmi_cat):
                                parse_dates=True,
                                index_col='UTC time')
             dane = dane.dropna(how='any')
-            dane.rename(columns={f'{sensor}_temperature': 'Temperatura', f'{sensor}_humidity': 'Wilgotnosc'
-                , f'{sensor}_pressure': 'Cisnienie', f'{sensor}_pm1': 'Stezenie PM1', f'{sensor}_pm25': 'Stezenie PM25',
-                                 f'{sensor}_pm10': 'Stezenie PM10'}, inplace=True)
+            dane.rename(columns={f'{sensor}_temperature': 'Temperature', f'{sensor}_humidity': 'Humidity'
+                , f'{sensor}_pressure': 'Pressure', f'{sensor}_pm1': 'Conc PM1', f'{sensor}_pm25': 'Conc PM25',
+                                 f'{sensor}_pm10': 'Conc PM10'}, inplace=True)
 
             dane = dane.astype(int)
-            X = dane[['Temperatura', 'Wilgotnosc', 'Cisnienie']]
-            y = dane[f'{pmi_cat}']
+            X = dane[['Temperature', 'Humidity', 'Pressure']]
+            y = dane[f'Conc {pmi_cat}']
 
             model = LinearRegression().fit(X, y)
             y_predict = model.predict(X)
@@ -194,12 +166,7 @@ def linear_regr(pmi_cat):
     print('(5/5) Procedure finished.')
     return r2_scorelist
 
-print('(1/2) Inititating procedure to calculate linear regression.')
-pmi_category = str(input('(2/2) Choose concentration metric for linear regression: Stezenie PM1, Stezenie PM25 ,Stezenie PM10'))
-r2_lin_scorelist = linear_regr(pmi_category)
-
-
-#8.2. REGRESJA WIELOMIANOWA
+#6.2. POLYNOMINAL REGRESSION
 def poly_regr(pmi_cat):
     r2_scorelist = {}
     r2_highest = {}
@@ -211,13 +178,13 @@ def poly_regr(pmi_cat):
                                parse_dates=True,
                                index_col='UTC time')
             dane = dane.dropna(how='any')
-            dane.rename(columns={f'{sensor}_temperature': 'Temperatura', f'{sensor}_humidity': 'Wilgotnosc'
-                , f'{sensor}_pressure': 'Cisnienie', f'{sensor}_pm1': 'Stezenie PM1', f'{sensor}_pm25': 'Stezenie PM25',
-                                 f'{sensor}_pm10': 'Stezenie PM10'}, inplace=True)
+            dane.rename(columns={f'{sensor}_temperature': 'Temperature', f'{sensor}_humidity': 'Humidity'
+                , f'{sensor}_pressure': 'Pressure', f'{sensor}_pm1': 'Conc PM1', f'{sensor}_pm25': 'Conc PM25',
+                                 f'{sensor}_pm10': 'Conc PM10'}, inplace=True)
 
             dane = dane.astype(int)
-            X = dane[['Temperatura', 'Wilgotnosc', 'Cisnienie']]
-            y = dane[f'{pmi_cat}']
+            X = dane[['Temperature', 'Humidity', 'Pressure']]
+            y = dane[f'Conc {pmi_cat}']
 
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             poly = PolynomialFeatures(degree=3)
@@ -246,7 +213,7 @@ def poly_regr(pmi_cat):
             print(f'(3/5) Overall average R2 score was: {mean_score} which is not satysfing.',
                   "(4/5) It's not enough for appropriate model.", sep='\n')
         else:
-            print(f'(0/5) Group of sensors which gave appropriate results: {r2_highest}')
+            print(f'(0/5) Group of sensors which gave satisfying results: {r2_highest}')
             print(f'(1/5) Highest prediction score was: {highest_score}.')
             print(f'(2/5) Lowest prediction score was: {lowest_score}.')
             print(f'(3/5) Overall average R2 score was: {mean_score} which is not satysfing.',
@@ -259,33 +226,13 @@ def poly_regr(pmi_cat):
     print('(5/5) Procedure finished.')
     return r2_scorelist
 
-print('(1/2) Inititating procedure to calculate polynomial regression.')
-pmi_category = str(input('(2/2) Choose concentration metric for polynomial regression: Stezenie PM1, Stezenie PM25 ,Stezenie PM10'))
-r2_poly_scorelist = poly_regr(pmi_category)
-
-#9 MERGOWANIE WYNIKÓW - NA MAPIE
-
-def drawing_result_map():
-    dataset = pd.DataFrame({'Linear': pd.Series(r2_lin_scorelist), 'Polynomial': pd.Series(r2_poly_scorelist)})
-    locations_path = fr"{filepath}sensor_locations.csv"
-    location_dataset = pd.read_csv(locations_path)
-
-    m = folium.Map(location=[50, 20], zoom_start=5, max_zoom=20)
-    counter = 0
-    while counter <= len(dataset) - 1:
-        folium.Marker(location=[location_dataset.iat[counter, 1], location_dataset.iat[counter, 2]],
-                      popup=f'Sensor nr: {location_dataset.iat[counter, 0]}, Linear regression R2: {dataset.iat[counter, 0]}, Polynomial regression R2: {dataset.iat[counter, 1]}',
-                      icon=folium.Icon()).add_to(m)
-        counter += 1
-
-    m.save(fr'{filepath}\sensors score\example.html')
-    webbrowser.open(fr'{filepath}\sensors score\example.html')
-
-drawing_result_map()
-
-#10. SIEC NEURONOWA
+#6.3. NEURON NETWORK
 #DO EWALUACJI
-'''os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+'''def neuron():
+    for sensor in real_sensors:
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 dane = pd.read_csv(fr"C:\\Users\Enter\OneDrive\Pulpit\smogData\sensors score\sensor170_mean.csv",
                                parse_dates=True,
                                index_col='UTC time')
@@ -340,8 +287,89 @@ model.fit(X_train, y_train, batch_size=500, epochs=5000, verbose=1, validation_d
 accuracy = model.evaluate(X_val, y_val,verbose  =0)
 print(accuracy)'''
 
+#7 MERGING THE RESULTS - ON THE MAP
+def drawing_result_map():
+    score_dataset = pd.DataFrame({'Linear': pd.Series(r2_lin_scorelist), 'Polynomial': pd.Series(r2_poly_scorelist)})
+    locations_path = fr"{filepath}sensor_locations.csv"
+    location_dataset = pd.read_csv(locations_path)
+
+    sensor_map = folium.Map(location=[50, 20], zoom_start=5, max_zoom=20)
+    counter = 0
+    while counter <= len(score_dataset) - 1:
+        folium.Marker(location=[location_dataset.iat[counter, 1], location_dataset.iat[counter, 2]],
+                      popup=f'SENSOR NUMBER: {location_dataset.iat[counter, 0]}, LINEAR REGRESSION R2: {score_dataset.iat[counter, 0]}, POLYNOMIAL REGRESSION R2: {score_dataset.iat[counter, 1]}',
+                      icon=folium.Icon()).add_to(sensor_map)
+        counter += 1
+    folium.TileLayer('Stamen Terrain').add_to(sensor_map)
+    folium.TileLayer('Stamen Toner').add_to(sensor_map)
+    folium.TileLayer('Stamen Water Color').add_to(sensor_map)
+    folium.TileLayer('cartodbpositron').add_to(sensor_map)
+    folium.TileLayer('cartodbdark_matter').add_to(sensor_map)
+    folium.LayerControl().add_to(sensor_map)
+    sensor_map.save(fr'{filepath}\sensors score\example.html')
+    webbrowser.open(fr'{filepath}\sensors score\example.html')
+
+
 #heatMAPA
 #dane usrednione dla miesiaca i wykaz najgorszych dla regionu?
 #wykres z dopuszczalna wartoscia - moze dodac zapisywanie danych dla ktorych bedzie wieksza od czegos?
 #wszystko wrzucic na wykres z heatmapa w formie info
 #sformulowac wnioski
+
+#8. PROGRAM LAUNCH
+
+print('(0/1) Welcome to the notebook in which we analyze smog readings in Cracow in 2017.',
+      'The program consists of functions:', '   - that reduce data from Kaggle and save them in a new format to disk,',
+      '    -that draws graphs for data taking into account WHO and Polish standards.',
+      '    -that calculates linear and polynomial regressions and places its results on an interactive map.', sep ='\n')
+decision_1 = str(input('(1/1) Do you wish to continue? Y/N '))
+while decision_1 == 'Y':
+
+    # 8.0. WORKING MONTH NAMES + PATH WHERE WE DOWNLOADED THE DATA
+    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+              'november', 'december']
+    filepath = fr"C:\\Users\Enter\OneDrive\Pulpit\smogData\\"
+
+    # 8.1. FINDING ALL SENSOR NAMES
+    print('(1/6) Looking for all unique sensor names.')
+    for month in months:
+        data = pd.read_csv(fr"{filepath}{month}-2017.csv")
+        sensors = []
+        for first_row_name in data.columns:
+            first_row_name
+            sensors.append(first_row_name[0:3])
+
+    sensors = pd.unique(sensors)
+    print('(2/6) All unique sensor names found.')
+
+    # 8.2. REPLACING SENSOR NAMES INTO INT
+    print('(3/6) Renaming sensors from string to integer.')
+    real_sensors = []
+    for value in sensors:
+        try:
+            real_sensors.append(int(value))
+        except ValueError:
+            pass
+    print('(4/6) Changing sensor names to integer values completed.',
+          '(5/6) Initiating program. Press appropriate button to initiate operation:',
+          '   1) FILES PREPERATION (Note: If you already have files on your local disk then you can skip this function)',
+          '   2) PLOTTING GRAPH FOR EVERY SENSOR (Note: Speaks for itself)',
+          '   3) PREDICTING (Note: Uses interactive map)', sep = '\n')
+    decision_2 = int(input())
+    if decision_2 == 1:
+        saving_month_results()
+        merging_year_data()
+        reducing_day_data()
+        deleting_sup_files()
+    elif decision_2 == 2:
+        print('(6/6) Plotting from data.')
+        plotting_concentration()
+    elif decision_2 == 3:
+        pmi_cat = str(input('(6/6) Choose concentration metric: PM1/PM2,5/PM10 (for PM2,5 type PM25):'))
+        r2_lin_scorelist = linear_regr(pmi_cat)
+        r2_poly_scorelist = poly_regr(pmi_cat)
+        drawing_result_map()
+    else:
+        print('Stopping program - incorrect operation chosen. Restart the program and choose from 1,2 or 3.')
+else:
+    print('Stopping program.')
